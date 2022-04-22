b77177cb118064ac8e94f2b3db822b54b376e9f2
sam <samueldazadaza@gmail.com>
2022-04-21 19:33:28 -0500

    35. rutas uso de useEffect

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Base = () => {
+    const [nombre, setNombre] = useState('pedro')
+
+    //para cargar contenido despues de 2seg de renderizar en el dom
+    useEffect ( () =>{
+        setTimeout( ()=>{
+            setNombre('Manuel')
+        }, 2000)
+    })
+
   return (
     <div>
         <h1>Pagina de Base ruta / </h1>
+        {nombre}
     </div>
   )
 }
